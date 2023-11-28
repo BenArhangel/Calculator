7ce366eb0f92f8fdbbca45ba814eebbec12ed215
BenArhangel <alex.konev.2222@gmail.com>
2023-11-28 16:43:25 +0300

    Initial commit. Added functions for all operators

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+function add(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+
+function subtract(firstNumber, secondNumber) {
+  console.log(firstNumber - secondNumber);
+}
+
+function multiply(firstNumber, secondNumber) {
+  console.log(firstNumber * secondNumber);
+}
+
+function divide(firstNumber, secondNumber) {
+  console.log(firstNumber / secondNumber);
+}
+
+function percentage(firstNumber, secondNumber) {
+  console.log((firstNumber / 100) * secondNumber);
+}
+
+add(10, 2);
+subtract(10, 2);
+multiply(10, 2);
+divide(10, 2);
+percentage(10, 2);

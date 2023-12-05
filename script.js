bdb661085b95fc65be6500126d5fa2890200a078
BenArhangel <alex.konev.2222@gmail.com>
2023-12-05 15:38:54 +0300

    Added condition when operator and number clicked

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 let storedValue = "";
+const firstValue = document.querySelector(".firstValue");
+const secondValue = document.querySelector(".secondValue");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const displayValue = document.querySelector(".value");
@@ -9,54 +11,72 @@ const displayResult = document.querySelector(".result");
 const equalSign = document.querySelector(".equal");
 
 function add(a, b) {
-  a + b;
+  displayResult.innerHTML = Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-  a - b;
+  displayResult.innerHTML = a - b;
 }
 
 function multiply(a, b) {
-  a * b;
+  displayResult.innerHTML = a * b;
 }
 
 function divide(a, b) {
-  a / b;
+  displayResult.innerHTML = a / b;
 }
 
 function percentage(a, b) {
-  (a / 100) * b;
+  displayResult.innerHTML = (a / 100) * b;
 }
 
 function operate(numOne, numTwo, operator) {
   switch (operator) {
     case "+":
       add(numOne, numTwo);
+      break;
     case "-":
       subtract(numOne, numTwo);
+      break;
+
     case "÷":
       divide(numOne, numTwo);
+      break;
+
     case "×":
       multiply(numOne, numTwo);
+      break;
+
     case "%":
       percentage(numOne, numTwo);
+      break;
   }
 }
 
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
-    storedValue += number.value;
-    displayValue.innerHTML = storedValue;
+    if (operator === "") {
+      firstNumber += number.value;
+      firstValue.innerHTML = firstNumber;
+    } else {
+      secondNumber += number.value;
+      secondValue.innerHTML = secondNumber;
+    }
   });
 });
 
 operators.forEach((op) => {
   op.addEventListener("click", () => {
-    storedValue += op.value;
-    displayValue.innerHTML = storedValue;
+    if (operator !== "") {
+      operator = op.value;
+      secondNumber.innerHTML += operator;
+    } else {
+      operator = op.value;
+      firstValue.innerHTML += operator;
+    }
   });
 });
 
 equalSign.addEventListener("click", () => {
-  console.log(operate(firstNumber, secondNumber, operator));
+  operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
 });
